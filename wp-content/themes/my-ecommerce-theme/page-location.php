421d065357671751ce699fb1ff8d7b8e1bc2773f
haksovanphanha99-dev <haksovanphanha99@gmail.com>
2026-01-11 11:07:51 +0700

    ok

---
+++ wp-content/themes/my-ecommerce-theme/page-location.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+<div class="location-hero">
+    <div class="logo">
+        <img src="<?= get_template_directory_uri(); ?>/img/2.jpg" alt="">
+    </div>
+    <h1>Our Location</h1>
+</div>
+<div class="location-all">
+    <div class="picture-of-location">
+        <img src="<?= get_template_directory_uri(); ?>/img/3.webp" alt="">
+        <img src="<?= get_template_directory_uri(); ?>/img/3.webp" alt="">
+        <img src="<?= get_template_directory_uri(); ?>/img/3.webp" alt="">
+        <img src="<?= get_template_directory_uri(); ?>/img/3.webp" alt="">
+        <img src="<?= get_template_directory_uri(); ?>/img/3.webp" alt="">
+        <img src="<?= get_template_directory_uri(); ?>/img/3.webp" alt="">
+    </div>
+</div>
+<?php get_footer(); ?>
