916d1f8872f2ac7410a4369e06a36db58e37725a
haksovanphanha99-dev <haksovanphanha99@gmail.com>
2026-01-12 13:11:39 +0700

    location-page

--- wp-content/themes/my-ecommerce-theme/page-location.php
+++ wp-content/themes/my-ecommerce-theme/page-location.php
@@ -8,10 +8,24 @@
 <div class="location-all">
     <div class="picture-of-location">
         <img src="<?= get_template_directory_uri(); ?>/img/3.webp" alt="">
+    </div>
+    <div class="picture-of-location">
+        <img src="<?= get_template_directory_uri(); ?>/img/4.webp" alt="">
+    </div>
+</div>
+<div class="location-all">
+    <div class="picture-of-location">
+        <img src="<?= get_template_directory_uri(); ?>/img/5.jpg" alt="">
+    </div>
+    <div class="picture-of-location">
         <img src="<?= get_template_directory_uri(); ?>/img/3.webp" alt="">
-        <img src="<?= get_template_directory_uri(); ?>/img/3.webp" alt="">
-        <img src="<?= get_template_directory_uri(); ?>/img/3.webp" alt="">
-        <img src="<?= get_template_directory_uri(); ?>/img/3.webp" alt="">
+    </div>
+</div>
+<div class="location-all">
+    <div class="picture-of-location">
+        <img src="<?= get_template_directory_uri(); ?>/img/g.jpg" alt="">
+    </div>
+    <div class="picture-of-location">
         <img src="<?= get_template_directory_uri(); ?>/img/3.webp" alt="">
     </div>
 </div>
