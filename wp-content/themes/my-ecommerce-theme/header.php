7c9de823795973d642b497f0e91d90e67b047258
haksovanphanha99-dev <haksovanphanha99@gmail.com>
2026-01-15 13:36:48 +0700

    add style css

--- wp-content/themes/my-ecommerce-theme/header.php
+++ wp-content/themes/my-ecommerce-theme/header.php
@@ -4,6 +4,7 @@
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
+   <link rel="stylesheet" href="wp_enqueue_style('mytheme-style', get_stylesheet_uri())/style.css">
     <?php wp_head(); ?>
 </head>
 
