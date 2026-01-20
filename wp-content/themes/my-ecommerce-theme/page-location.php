26aa4f8195ee5f12957400bf74ac4858484da555
haksovanphanha99-dev <haksovanphanha99@gmail.com>
2026-01-20 07:51:37 +0700

    merge

---
+++ wp-content/themes/my-ecommerce-theme/page-location.php
@@ -0,0 +1,32 @@
+<?php get_header(); ?>
+<div class="location-hero">
+    <div class="logo">
+        <img src="<?= get_template_directory_uri(); ?>/img/2.jpg" alt="">
+    </div>
+    <h1>Our Location</h1>
+</div>
+<div class="location-all">
+    <div class="picture-of-location">
+        <img src="<?= get_template_directory_uri(); ?>/img/3.webp" alt="">
+    </div>
+    <div class="picture-of-location">
+        <img src="<?= get_template_directory_uri(); ?>/img/4.webp" alt="">
+    </div>
+</div>
+<div class="location-all">
+    <div class="picture-of-location">
+        <img src="<?= get_template_directory_uri(); ?>/img/5.jpg" alt="">
+    </div>
+    <div class="picture-of-location">
+        <img src="<?= get_template_directory_uri(); ?>/img/3.webp" alt="">
+    </div>
+</div>
+<div class="location-all">
+    <div class="picture-of-location">
+        <img src="<?= get_template_directory_uri(); ?>/img/g.jpg" alt="">
+    </div>
+    <div class="picture-of-location">
+        <img src="<?= get_template_directory_uri(); ?>/img/3.webp" alt="">
+    </div>
+</div>
+<?php get_footer(); ?>
